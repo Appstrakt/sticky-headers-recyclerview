11145eca293cc307d6535129bccf5bca1af94974
Vincent de Smet <vincent.desmet@appstrakt.com>
2015-10-08 15:12:06 +0200

    Reformat code
    Signed-off-by: Vincent de Smet <vincent.desmet@appstrakt.com>

--- library/src/main/java/com/timehop/stickyheadersrecyclerview/rendering/HeaderRenderer.java
+++ library/src/main/java/com/timehop/stickyheadersrecyclerview/rendering/HeaderRenderer.java
@@ -20,76 +20,78 @@ import com.timehop.stickyheadersrecyclerview.util.OrientationProvider;
  */
 public class HeaderRenderer {
 
-  private final DimensionCalculator mDimensionCalculator;
-  private final OrientationProvider mOrientationProvider;
+    private final DimensionCalculator mDimensionCalculator;
+    private final OrientationProvider mOrientationProvider;
 
-  /**
-   * The following field is used as a buffer for internal calculations. Its sole purpose is to avoid
-   * allocating new Rect every time we need one.
-   */
-  private final Rect mTempRect = new Rect();
-  private final Rect mRect;
-  private Paint mPaint;
+    /**
+     * The following field is used as a buffer for internal calculations. Its sole purpose is to
+     * avoid
+     * allocating new Rect every time we need one.
+     */
+    private final Rect mTempRect = new Rect();
+    private final Rect mRect;
+    private Paint mPaint;
 
-  public HeaderRenderer(OrientationProvider orientationProvider) {
-    this(orientationProvider, new DimensionCalculator());
-  }
-
-  private HeaderRenderer(OrientationProvider orientationProvider,
-      DimensionCalculator dimensionCalculator) {
-    mOrientationProvider = orientationProvider;
-    mDimensionCalculator = dimensionCalculator;
-    mRect = new Rect();
-  }
-
-  /**
-   * Draws a header to a canvas, offsetting by some x and y amount
-   *
-   * @param recyclerView the parent recycler view for drawing the header into
-   * @param canvas       the canvas on which to draw the header
-   * @param header       the view to draw as the header
-   * @param offset       a Rect used to define the x/y offset of the header. Specify x/y offset by setting
-   *                     the {@link Rect#left} and {@link Rect#top} properties, respectively.
-   */
-  public void drawHeader(RecyclerView recyclerView, Canvas canvas, View header, Rect offset) {
-    canvas.save();
-
-    if (recyclerView.getLayoutManager().getClipToPadding()) {
-      // Clip drawing of headers to the padding of the RecyclerView. Avoids drawing in the padding
-      initClipRectForHeader(mTempRect, recyclerView, header);
-      canvas.clipRect(mTempRect);
+    public HeaderRenderer(OrientationProvider orientationProvider) {
+        this(orientationProvider, new DimensionCalculator());
     }
 
-    canvas.translate(offset.left, offset.top);
-
-    header.draw(canvas);
-
-    if (offset.top == recyclerView.getPaddingTop() && offset.left == recyclerView
-        .getPaddingLeft()) {
-
-        if (mPaint == null) {
-            initPaint(recyclerView);
-        }
-        final Resources r = recyclerView.getResources();
-        final int px = (int) TypedValue
-            .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, r.getDisplayMetrics());
-
-        if (mOrientationProvider.getOrientation(recyclerView) == OrientationHelper.VERTICAL) {
-            mRect.top = header.getBottom();
-            mRect.left = header.getLeft();
-            mRect.bottom = mRect.top + (int)(px * 0.6f);
-            mRect.right = header.getRight();
-        } else {
-            mRect.top = header.getTop();
-            mRect.left = header.getRight();
-            mRect.bottom = header.getBottom();
-            mRect.right = header.getRight() + px;
-        }
-
-        canvas.drawRect(mRect, mPaint);
+    private HeaderRenderer(OrientationProvider orientationProvider,
+        DimensionCalculator dimensionCalculator) {
+        mOrientationProvider = orientationProvider;
+        mDimensionCalculator = dimensionCalculator;
+        mRect = new Rect();
     }
-      canvas.restore();
-  }
+
+    /**
+     * Draws a header to a canvas, offsetting by some x and y amount
+     *
+     * @param recyclerView the parent recycler view for drawing the header into
+     * @param canvas       the canvas on which to draw the header
+     * @param header       the view to draw as the header
+     * @param offset       a Rect used to define the x/y offset of the header. Specify x/y offset by
+     *                     setting
+     *                     the {@link Rect#left} and {@link Rect#top} properties, respectively.
+     */
+    public void drawHeader(RecyclerView recyclerView, Canvas canvas, View header, Rect offset) {
+        canvas.save();
+
+        if (recyclerView.getLayoutManager().getClipToPadding()) {
+            // Clip drawing of headers to the padding of the RecyclerView. Avoids drawing in the padding
+            initClipRectForHeader(mTempRect, recyclerView, header);
+            canvas.clipRect(mTempRect);
+        }
+
+        canvas.translate(offset.left, offset.top);
+
+        header.draw(canvas);
+
+        if (offset.top == recyclerView.getPaddingTop() && offset.left == recyclerView
+            .getPaddingLeft()) {
+
+            if (mPaint == null) {
+                initPaint(recyclerView);
+            }
+            final Resources r = recyclerView.getResources();
+            final int px = (int) TypedValue
+                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, r.getDisplayMetrics());
+
+            if (mOrientationProvider.getOrientation(recyclerView) == OrientationHelper.VERTICAL) {
+                mRect.top = header.getBottom();
+                mRect.left = header.getLeft();
+                mRect.bottom = mRect.top + (int) (px * 0.6f);
+                mRect.right = header.getRight();
+            } else {
+                mRect.top = header.getTop();
+                mRect.left = header.getRight();
+                mRect.bottom = header.getBottom();
+                mRect.right = header.getRight() + px;
+            }
+
+            canvas.drawRect(mRect, mPaint);
+        }
+        canvas.restore();
+    }
 
     private void initPaint(RecyclerView recyclerView) {
         mPaint = new Paint();
@@ -103,38 +105,35 @@ public class HeaderRenderer {
 
         final float dx = isHorizontal ? px : 0;
         final float dy = isHorizontal ? 0 : px;
-        final LinearGradient lg = new LinearGradient(dx, 0, 0, dy, //
-            new int[] {Color.TRANSPARENT, 0x80000000},  //
+        final LinearGradient lg = new LinearGradient(0, 0, dx, dy, //
+            new int[] {0x80000000, Color.TRANSPARENT},  //
             new float[] {0, 1}, Shader.TileMode.MIRROR);
         mPaint.setShader(lg);
     }
 
     /**
-   * Initializes a clipping rect for the header based on the margins of the header and the padding of the
-   * recycler.
-   * FIXME: Currently right margin in VERTICAL orientation and bottom margin in HORIZONTAL
-   * orientation are clipped so they look accurate, but the headers are not being drawn at the
-   * correctly smaller width and height respectively.
-   *
-   * @param clipRect {@link Rect} for clipping a provided header to the padding of a recycler view
-   * @param recyclerView for which to provide a header
-   * @param header       for clipping
-   */
-  private void initClipRectForHeader(Rect clipRect, RecyclerView recyclerView, View header) {
-    mDimensionCalculator.initMargins(clipRect, header);
-    if (mOrientationProvider.getOrientation(recyclerView) == LinearLayout.VERTICAL) {
-      clipRect.set(
-          recyclerView.getPaddingLeft(),
-          recyclerView.getPaddingTop(),
-          recyclerView.getWidth() - recyclerView.getPaddingRight() - clipRect.right,
-          recyclerView.getHeight() - recyclerView.getPaddingBottom());
-    } else {
-        clipRect.set(
-          recyclerView.getPaddingLeft(),
-          recyclerView.getPaddingTop(),
-          recyclerView.getWidth() - recyclerView.getPaddingRight(),
-          recyclerView.getHeight() - recyclerView.getPaddingBottom() - clipRect.bottom);
+     * Initializes a clipping rect for the header based on the margins of the header and the padding
+     * of the
+     * recycler.
+     * FIXME: Currently right margin in VERTICAL orientation and bottom margin in HORIZONTAL
+     * orientation are clipped so they look accurate, but the headers are not being drawn at the
+     * correctly smaller width and height respectively.
+     *
+     * @param clipRect     {@link Rect} for clipping a provided header to the padding of a recycler
+     *                     view
+     * @param recyclerView for which to provide a header
+     * @param header       for clipping
+     */
+    private void initClipRectForHeader(Rect clipRect, RecyclerView recyclerView, View header) {
+        mDimensionCalculator.initMargins(clipRect, header);
+        if (mOrientationProvider.getOrientation(recyclerView) == LinearLayout.VERTICAL) {
+            clipRect.set(recyclerView.getPaddingLeft(), recyclerView.getPaddingTop(),
+                recyclerView.getWidth() - recyclerView.getPaddingRight() - clipRect.right,
+                recyclerView.getHeight() - recyclerView.getPaddingBottom());
+        } else {
+            clipRect.set(recyclerView.getPaddingLeft(), recyclerView.getPaddingTop(),
+                recyclerView.getWidth() - recyclerView.getPaddingRight(),
+                recyclerView.getHeight() - recyclerView.getPaddingBottom() - clipRect.bottom);
+        }
     }
-  }
-
 }
